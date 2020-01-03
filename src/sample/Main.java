4e4db5bd14612c2289cf1468add5ca12b24627ab
eskioglou <eskioglou@csd.auth.gr>
2020-01-03 14:13:08 +0200

    Deleted classes {Registration, users1, Welcome}. Fixed login,registration and anonymous system. login.txt added.

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,16 +1,84 @@
 package sample;
 
 
+import java.io.*;
+import java.util.Formatter;
+import java.util.Scanner;
 
 class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Welcome wel= new Welcome();
+        Scanner s1, s2;
+        //int number = 0;
+        System.out.println("-----|Welcome|-----");
+        System.out.println("Registration: Press 1");
+        System.out.println("Login: Press 2");
+        System.out.println("Anonymous: Press 3");
+        Scanner scanner1 = new Scanner(System.in);
+        int number = scanner1.nextInt();
 
-        //Interface inter = new Interface();
-        //inter.start();
 
+        //Registration
+        if (number == 1) {
+            System.out.println("Registration Page");
+            PrintWriter pw = new PrintWriter(new FileWriter("login.txt", true));
+            Scanner scanner = new Scanner(System.in);
+            String text = "";
+            System.out.println("Username: ");
+            text = scanner.next();
+            pw.write(text + ",");
+            System.out.println("Password: ");
+            text = scanner.next();
+            pw.write(text);
+            pw.println();
+            pw.close();
+            System.out.println("Registration Successful");
+            new SecondWindow();
+        }
+
+        //Login
+        if (number == 2) {
+            Scanner x;
+            System.out.println("Username: ");
+            Scanner scanner2= new Scanner(System.in);
+            String n= scanner2.next();
+            System.out.println("Password: ");
+            Scanner scanner3= new Scanner(System.in);
+            String m= scanner3.next();
+
+                boolean found= false;
+                String tempUsername= "";
+                String tempPassword= "";
+
+                try{
+                    x= new Scanner(new File("login.txt"));
+                    x.useDelimiter("[,\n]");
+
+                    if (x.hasNext() && !found){
+                        tempUsername=x.next();
+                        tempPassword=x.next();
+
+                        if(tempUsername.trim().equals(n.trim()) && tempPassword.trim().equals(m.trim())){
+                            System.out.println("You are logged in.");
+                            new SecondWindow();
+                        }
+                        else {
+                            System.out.println("Invalid user or Wrong Password");
+                        }
+                    }
+                    x.close();
+                }
+                catch (Exception e){
+                    System.out.println("Invalid User or Wrong Password.");
+                }
+            }
+
+
+        else if (number == 3) {
+            System.out.println("You are logged in as Anonymous.");
+            new SecondWindow();
+        }
     }
-
-
 }
+
+
