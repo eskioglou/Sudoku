2458b1842b053f5ec9cd8ae91eac02589f0472ba
eskioglou <eskioglou@csd.auth.gr>
2020-01-04 17:28:19 +0200

    Image added.

--- src/sample/SecondWindow.java
+++ src/sample/SecondWindow.java
@@ -1,67 +1,62 @@
 package sample;
+
 import javax.imageio.ImageIO;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
 import java.awt.*;
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 
-public class SecondWindow {
-    private JFrame frame= new JFrame();
-    private JPanel panel;
-    private JButton sudokubutton= new JButton();
-    private JButton killerbutton= new JButton();
-    private JButton duidokubutton= new JButton();
-    private JLabel label= new JLabel();
+public class SecondWindow
+{
 
-
+    MyDrawPanel draw;
     public SecondWindow(){
-        makeFrame();
+        go();
+
     }
-    void makeFrame(){
-        frame.setTitle("Sudoku Game");
-        frame.setSize(600,500);
-        frame.setResizable(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        sudokubutton = new JButton();
-        sudokubutton.setText("Sudoku");
-        sudokubutton.setActionCommand("Like");
-        sudokubutton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-            frame.dispose();
-            SudokuFrame f= new SudokuFrame(9);
-        }
-
-        });
-
-        killerbutton = new JButton();
-        killerbutton.setText("Killer Sudoku");
-        killerbutton.setActionCommand("Like");
-        killerbutton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                frame.dispose();
-                SudokuFrame f= new SudokuFrame(9);
-            }
-        });
-
-        duidokubutton = new JButton();
-        duidokubutton.setText("Duidoku");
-        duidokubutton.setActionCommand("Like");
-        duidokubutton.addActionListener(actionEvent -> {
-            frame.dispose();
-            DuidokuFrame f=new DuidokuFrame(4);
-        });
+     public void go()
+    {
+        JFrame frame=new JFrame("Sudoku Game");
         //Center View
         Toolkit t = Toolkit.getDefaultToolkit();
         Dimension d = t.getScreenSize();
         int x = (d.width-frame.getWidth())/2;
         int y = (d.height-frame.getHeight())/2;
         frame.setLocation(x, y);
+
+        JPanel panel=new JPanel();
+        draw=new MyDrawPanel();
+        JButton sudoku = new JButton("Sudoku");
+        sudoku.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                //frame.dispose();
+                new SudokuFrame(9);
+            }
+        });
+        JButton killer= new JButton("Killer Sudoku");
+        killer.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                //frame.dispose();
+                new SudokuFrame(9);
+            }
+        });
+        JButton duidoku= new JButton("Duidoku");
+        duidoku.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                //frame.dispose();
+                new DuidokuFrame(4);
+            }
+        });
+
+
 
         //Set Image Icon
         ImageIcon icon= new ImageIcon();
@@ -72,20 +67,40 @@ public class SecondWindow {
             System.out.println("When reading icon file: " + ex.getMessage());
         }
 
+        //Set Audio to help concentration.
+        try {
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(Main.class.getResource("Chopin - Spring Waltz.wav"));
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioIn);
+            clip.start();
+        } catch(Exception ex) {
+            System.out.println("Error with playing sound.");
+            ex.printStackTrace();
+        }
 
-        label.setText("Choose a version: ");
-        panel=new JPanel();
-        panel.add(label);
-        panel.add(sudokubutton);
-        panel.add(killerbutton);
-        panel.add(duidokubutton);
 
-
-
-
-        frame.add(panel);
-
+        draw.add(sudoku);
+        draw.add(killer);
+        draw.add(duidoku);
+        frame.getContentPane().add(BorderLayout.CENTER,draw);
+        panel.add(sudoku);
+        panel.add(killer);
+        panel.add(duidoku);
+        frame.getContentPane().add(panel,BorderLayout.NORTH);
+        frame.setSize(600,500);
         frame.setVisible(true);
     }
+    public class MyDrawPanel extends JPanel
+    {
+        @Override
+        public void paintComponent(Graphics g)
+        {
+            Image image=new ImageIcon("src/sample/512x512bb.jpg").getImage();
+            g.drawImage(image,90,0,400,400,this);
+
+        }
+
+    }
+
 
 }
